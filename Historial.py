acc889198eaf650bc79054838eed59082b5e8800
BasilioSalazarN <116610544+BasilioSalazarN@users.noreply.github.com>
2023-03-01 15:17:41 -0600

    Update Historial.py

--- Historial.py
+++ Historial.py
@@ -1,7 +1,8 @@
+# ----- Librerías ---- #
 import numpy as np
 import streamlit as st
 import pandas as pd
-#import psycopg2
+import psycopg2-binary
 from urllib.parse import urlparse
 
 uri=st.secrets.db_credentials.URI
@@ -11,7 +12,7 @@ database = result.path[1:]
 username = result.username
 pwd = result.password
 port_id = result.port
-con = psycopg2.connect(host=hostname,dbname= database,user= username,password=pwd,port= port_id)
+con = psycopg2-binary.connect(host=hostname,dbname= database,user= username,password=pwd,port= port_id)
 
 data_1_r=pd.read_sql(f"select cast(id as integer),marca,usuario,nombre,horario,puesto,supervisor,proceso,fecha,bloque,estado,tipo,cast(predios as integer),cast(horas as float) from registro", con)
 data_2_r = data_1_r.groupby(["nombre", "fecha"], as_index=False)["horas"].agg(np.sum)
@@ -29,7 +30,6 @@ nombre_o=data_2_o.iloc[:,0]
 fecha_r=data_2_r.iloc[:,0]
 fecha_c=data_2_c.iloc[:,0]
 fecha_o=data_2_o.iloc[:,0]
-
 
 nombre=str.cat(nombre_r,nombre_c,nombre_o)
 
